904926f952d855fd9f8255aa1d2058d4c3a2b092
csiyang <csiyang@yahoo.co.nz>
2020-09-15 14:04:58 +1200

    Added line #3

--- A2PartD/src/main.java
+++ A2PartD/src/main.java
@@ -4,6 +4,7 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("My name is Siyang.");
 		System.out.println("My favourite colour is orange.");
+		System.out.println("My favourite mode of transportation is driving.");
 
 	}
 
