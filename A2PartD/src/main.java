fa787f982bf943c1dae8fb88de127e24d4ac63d1
csiyang <csiyang@yahoo.co.nz>
2020-09-15 14:06:18 +1200

    Added line #4

--- A2PartD/src/main.java
+++ A2PartD/src/main.java
@@ -5,6 +5,7 @@ public class main {
 		System.out.println("My name is Siyang.");
 		System.out.println("My favourite colour is orange.");
 		System.out.println("My favourite mode of transportation is driving.");
+		System.out.println("The best day of the week is Wednesday");
 
 	}
 
